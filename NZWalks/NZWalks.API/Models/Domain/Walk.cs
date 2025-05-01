8179e74f4d557d4d9d72584e7a0fa9e58cfabd50
mohsen8yousef <ywsfmhsn363@gmail.com>
2025-05-02 00:48:16 +0300

    end of s 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDefficultyId { get; set; }
+
+        //Navigation Properties
+        public Region Region { get; set; }
+        public WalkDefficulty WalkDefficulty { get; set; }
+
+    }
+}
